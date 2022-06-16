060bd85f876dacb607f22ae3a4cd65576bcbebcb
kerstin.gerull <kerstin.gerull@basilicom.de>
2022-06-16 12:47:19 +0200

    fix error when opening a folder it was checking for data quality, which threw an error
    added a reload button for the data quality tab in a data object

--- src/Resources/public/js/DataQualityBundle.js
+++ src/Resources/public/js/DataQualityBundle.js
@@ -9,7 +9,7 @@ pimcore.plugin.DataQualityBundle = Class.create(pimcore.plugin.admin, {
     pimcore.plugin.broker.registerPlugin(this);
   },
 
-  prepareClassLayoutContextMenu: function (allowedTypes, source) {
+  prepareClassLayoutContextMenu: function (allowedTypes) {
     for (let layout in allowedTypes) {
       if (allowedTypes[layout] !== undefined && allowedTypes[layout].length > 0) {
         allowedTypes[layout].push('dataQuality')
@@ -19,22 +19,43 @@ pimcore.plugin.DataQualityBundle = Class.create(pimcore.plugin.admin, {
     return allowedTypes;
   },
 
-  postOpenObject: function (object, type) {
-    if (object.data.general.o_classId !== 'DQC') {
+  postOpenObject: function (object) {
+    if (object.data.general.o_classId !== 'DQC' && object.data.general.o_type === 'object') {
       fetch('/admin/data-quality/check-class-has-data-quality/' + object.id)
         .then((response) => {
           if (response.status === 200) {
             var path = '/admin/data-quality/index/' + object.id;
-            var tab = Ext.create('Ext.panel.Panel', {
+            var html = '<iframe src="' + path + '" style="width: 100%; height: 100%; border: 0;"></iframe>';
+
+            this.tab = Ext.create('Ext.panel.Panel', {
               border: false,
               autoScroll: true,
               closable: false,
               iconCls: 'pimcore_icon_charty',
               bodyCls: 'pimcore_overflow_scrolling',
-              html: '<iframe src="' + path + '" style="width: 100%; height: 100%;" frameborder="0"></iframe>',
+              html: html,
+              tbar: {
+                items: [
+                  '->', // Fill
+                  {
+                    xtype: 'button',
+                    text: t('refresh'),
+                    iconCls: 'pimcore_icon_reload',
+                    handler: function () {
+                      var key = "object_" + object.id;
+                      if (pimcore.globalmanager.exists(key)) {
+                        var objectTab = pimcore.globalmanager.get(key);
+                        objectTab.saveToSession(function () {
+                          this.tab.setHtml(html);
+                        }.bind(this));
+                      }
+                    }.bind(this)
+                  }
+                ]
+              }
             });
             // this adds the pimcore tab
-            object.tabbar.add([tab]);
+            object.tabbar.add([this.tab]);
             pimcore.layout.refresh();
           }
         })
