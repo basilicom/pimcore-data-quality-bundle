ba767c854711104537450228d5600de7b4e58af9
alexander.heidrich <alexander.heidrich@basilicom.de>
2023-08-15 10:11:53 +0200

    bugfix: Pimcore 11 fixes for reports, grid view, js extensions and installer

--- src/Controller/DataQualityController.php
+++ src/Controller/DataQualityController.php
@@ -11,6 +11,7 @@ use Pimcore\Translation\Translator;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/admin/data-quality")
